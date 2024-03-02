13dfe3294333cf807060730112ac5b43093f7434
Erlan-asd <Vipoffacc@gmail.com>
2024-03-02 18:04:53 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,37 @@
+// 1)-----------------------------------------------------------
+// const brandsArr = ['google', 'apple', 'microsoft']
+// console.log(brandsArr);
+// 1-способ
+// brandsArr[3] = 'xiaomi';
+// brandsArr[4] = 'huawei';
+// brandsArr[5] = 'oneplus';
+// console.log(brandsArr);
+// 2-способ
+// brandsArr.push('xiaomi', 'huawei', 'oneplus')
+// console.log(brandsArr);
+// 3-способ
+// brandsArr.splice(3,0,'xiaomi', 'huawei', 'oneplus')
+// console.log(brandsArr);
+
+
+// 2)---------------------------------------------------------------
+// const names = ['Ernest', 'Kanimet', 'Bakai', 'Ulan', 'Erlan'];
+// let name1 = prompt('Введите имя в конец');
+// let name2 = prompt('Введите имя в начало');
+// names.push(name1);
+// names.unshift(name2);
+// console.log(names);
+
+
+// 3)------------------------------------------------------------------
+// const colors = ['red','yellow','blue','purple','black','white'];
+// console.log(colors);
+// colors.splice(1, 1,'green');
+// console.log(colors);
+
+
+// 4)-----------------------------------------------------------------
+// const numbers = [1,2,3,4,5,6,7,8,9,10];
+// console.log(numbers);
+// const sliced = numbers.slice(4,8);
+// console.log(sliced);
